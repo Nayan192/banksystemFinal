9b064a409fe8f74c3cae93b58a3e3453503c61e7
Nayan192 <64128354+Nayan192@users.noreply.github.com>
2021-11-01 18:52:44 +0530

    Add files via upload

--- createUser.php
+++ createUser.php
@@ -27,11 +27,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/createUser.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <?php require 'partials/navbar.php'?>
     <div class="background">
     <h1 class="mainheading">CREATE NEW USER</h1><br>
     <?php
